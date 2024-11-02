59860e83e7e79394f71f119497af879c7127efa3
Jnanamithran M <jnanamithranm@gmail.com.com>
2024-11-02 23:17:41 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type",{
-    strings : ["a Programmer","a Developer","a Editor","Jnanamithran M"],
+    strings : ["a Programmer.","a Developer.","a Editor.","Jnanamithran M."],
     typeSpeed : 150,
     backSpeed : 150,
     looped : true
