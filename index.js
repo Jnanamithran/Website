1ffcb9ef57492854d3e967226e2c46110312419e
Jnanamithran M <jnanamithranm@gmail.com>
2025-02-02 15:09:04 +0530

    Lot of chnages

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var typed = new Typed(".auto-type",{
-    strings : ["a Programmer.","a Developer.","an Editor.","Jnanamithran M."],
-    typeSpeed : 150,
-    backSpeed : 150,
-    looped : true
-})
+var typed = new Typed(".auto-type", {
+    strings: ["a Programmer.", "a Developer.", "an Editor.", "Jnanamithran M."],
+    typeSpeed: 150,
+    backSpeed: 150,
+    backDelay: 1000,  // Delay before starting to backspace
+    startDelay: 500,  // Delay before typing starts
+    loop: true,       // Ensures the typing effect repeats
+    showCursor: false // Hide cursor for a cleaner effect
+  });
+  
